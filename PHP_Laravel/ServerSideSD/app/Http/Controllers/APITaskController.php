30ab3eb752365b1b0d458dfb2eb8426d43385cef
filipafaraujo <filipa.fonseca.araujo@gmail.com>
2025-02-14 09:10:33 +0000

    finished version

--- PHP_Laravel/ServerSideSD/app/Http/Controllers/APITaskController.php
+++ PHP_Laravel/ServerSideSD/app/Http/Controllers/APITaskController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Resources\TaskResource;
+use App\Http\Resources\TaskResourceCollection;
 
 class APITaskController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index() : TaskResourceCollection
     {
-        //
+        return new TaskResourceCollection(resource:Task::paginate());
     }
 
     /**
@@ -28,15 +30,23 @@ class APITaskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'user_id' => 'required'
+        ]);
+
+        Task::create($request->all());
+
+        return response()->json('tarefa adicionada com sucesso!');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Task $task)
+    public function show(Task $task) : TaskResource
     {
-        return $task;
+        return new TaskResource($task);
     }
 
     /**
@@ -50,16 +60,19 @@ class APITaskController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Task $task)
     {
-        //
+        $task->update($request->all());
+        return response()->json('actualizado com sucesso');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->json('apagado com sucesso!');
     }
 }
--- PHP_Laravel/ServerSideSD/routes/api.php
+++ PHP_Laravel/ServerSideSD/routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/task/{task}', [APITaskController::class, 'show']);
+Route::apiResource('/task', APITaskController::class);
